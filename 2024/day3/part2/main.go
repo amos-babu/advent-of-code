2c13f2c6c8056a4889a2e86e4585c707cdf961d6
amos-babu <amos.babu@yahoo.com>
2025-05-26 14:22:55 +0300

    update day 3 part 2

--- 2024/day3/part2/main.go
+++ 2024/day3/part2/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"regexp"
 	"strconv"
@@ -10,33 +11,69 @@ import (
 func main() {
 	file, err := os.Open("input.txt")
 	checkErr(err)
-
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-
 	total := 0
+	enabled := true
+
+	mulRe := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	doRe := regexp.MustCompile(`\bdo\(\)`)
+	dontRe := regexp.MustCompile(`don't\(\)`)
+
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+		doLocs := doRe.FindAllStringIndex(line, -1)
+		dontLocs := dontRe.FindAllStringIndex(line, -1)
 
-		matches := re.FindAllStringSubmatch(line, -1)
-
-		for _, match := range matches {
-			num1, err1 := strconv.Atoi(match[1])
-			checkErr(err1)
-			num2, err2 := strconv.Atoi(match[2])
-			checkErr(err2)
-
-			totalMatch := num1 * num2
-			total += totalMatch
+		type toggle struct {
+			pos    int
+			enable bool
+		}
+		var toggles []toggle
+		for _, loc := range doLocs {
+			toggles = append(toggles, toggle{loc[0], true})
+		}
+		for _, loc := range dontLocs {
+			toggles = append(toggles, toggle{loc[0], false})
 		}
 
-	}
-	checkErr(scanner.Err())
+		for i := 0; i < len(toggles)-1; i++ {
+			for j := i + 1; j < len(toggles); j++ {
+				if toggles[i].pos > toggles[j].pos {
+					toggles[i], toggles[j] = toggles[j], toggles[i]
+				}
+			}
+		}
 
-	println(total)
+		mulMatches := mulRe.FindAllStringSubmatchIndex(line, -1)
+		currToggle := 0
+
+		for _, match := range mulMatches {
+			start := match[0]
+
+			for currToggle < len(toggles) && toggles[currToggle].pos <= start {
+				enabled = toggles[currToggle].enable
+				currToggle++
+			}
+
+			if !enabled {
+				continue
+			}
+
+			num1Str := line[match[2]:match[3]]
+			num2Str := line[match[4]:match[5]]
+			num1, err1 := strconv.Atoi(num1Str)
+			num2, err2 := strconv.Atoi(num2Str)
+			checkErr(err1)
+			checkErr(err2)
+
+			total += num1 * num2
+		}
+	}
+
+	fmt.Println("Part Two total:", total)
 }
 
 func checkErr(err error) {
