8a6df465977672f9ef2d4cfb0c83f8c1d0f77c11
amos-babu <amos.babu@yahoo.com>
2025-05-28 16:23:18 +0300

    day 4 part 1

--- 2024/day4/part1/main.go
+++ 2024/day4/part1/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	file, err := os.Open("demo.txt")
+	checkErr(err)
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var grid [][]rune
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		grid = append(grid, []rune(line))
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(grid[0][1]))
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+
+	}
 }
