a6f2cf90bb22db064a037a8a3ff6f61244bec08d
Babu Amos <amos.babu@yahoo.com>
2025-12-05 00:36:18 +0300

    day 2 part 1

--- 2025/day2/part1/main.go
+++ 2025/day2/part1/main.go
@@ -1,3 +1,74 @@
 package main
 
-func main() {}
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("../input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	var numbers []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		u := strings.FieldsFunc(line, separator)
+		for _, num := range u {
+			numbers = append(numbers, num)
+		}
+	}
+
+	totalInvalid := 0
+
+	for _, value := range numbers {
+		strs := strings.FieldsFunc(value, dashSeparator)
+		ints := make([]int, len(strs))
+		for i, s := range strs {
+			n, err := strconv.Atoi(s)
+			if err != nil {
+				log.Fatal(err)
+			}
+			ints[i] = n
+		}
+
+		for i := ints[0]; i <= ints[1]; i++ {
+			s := strconv.Itoa(i)
+
+			if len(s)%2 != 0 {
+				continue
+			}
+			mid := len(s) / 2
+			left := s[:mid]
+			right := s[mid:]
+
+			if left == right {
+				n, err := strconv.Atoi(s)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				totalInvalid += n
+			}
+		}
+
+		fmt.Println(totalInvalid)
+	}
+}
+
+func dashSeparator(r rune) bool {
+	return r == '-'
+}
+
+func separator(r rune) bool {
+	return r == ','
+}
