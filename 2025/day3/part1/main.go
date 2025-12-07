4a86f1484945ac59cc3e736e86b182015fec5cb4
Babu Amos <amos.babu@yahoo.com>
2025-12-07 09:42:56 +0300

    day 3 done!

--- 2025/day3/part1/main.go
+++ 2025/day3/part1/main.go
@@ -1,5 +1,62 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
 func main() {
+	f, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	total := 0
+
+	for scanner.Scan() {
+		var batteriesLine []int
+		line := scanner.Text()
+		for _, v := range line {
+			b := int(v - '0')
+			batteriesLine = append(batteriesLine, b)
+		}
+
+		max := batteriesLine[0]
+		maxId := 0
+
+		for i, v := range batteriesLine[:len(batteriesLine)-1] {
+			if v > max {
+				max = v
+				maxId = i
+			}
+
+		}
+
+		secondMax := batteriesLine[maxId+1]
+		for _, v := range batteriesLine[maxId+1:] {
+
+			if v > secondMax {
+				secondMax = v
+
+			}
+		}
+
+		bank, err := strconv.Atoi(strconv.Itoa(max) + strconv.Itoa(secondMax))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		total += bank
+
+		// break
+	}
+
+	fmt.Println(total)
 
 }
--- 2025/day3/part2/main.go
+++ 2025/day3/part2/main.go
@@ -1,5 +1,61 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
 func main() {
+	f, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	total := 0
+
+	for scanner.Scan() {
+		var batteriesLine []int
+		line := scanner.Text()
+		for _, v := range line {
+			b := int(v - '0')
+			batteriesLine = append(batteriesLine, b)
+		}
+
+		numDigits := 12
+		start := 0
+		maxNum := ""
+
+		for k := 0; k < numDigits; k++ {
+			remaining := numDigits - k
+			end := len(batteriesLine) - remaining
+
+			bestDigit := -1
+			bestIndex := start
+
+			for i := start; i <= end; i++ {
+				if batteriesLine[i] > bestDigit {
+					bestDigit = batteriesLine[i]
+					bestIndex = i
+				}
+			}
+
+			maxNum += strconv.Itoa(bestDigit)
+			start = bestIndex + 1
+		}
+		bank, err := strconv.Atoi(maxNum)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		total += bank
+	}
+
+	fmt.Println(total)
 
 }
